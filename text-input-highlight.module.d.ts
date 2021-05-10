e489c5160c5306e1dbe6ec0e8374d95afa0da4af
Seiya Tashiro <seiya.tashiro@little-method.com>
2021-05-10 10:10:34 +0900

    #NG-23 NgModuleを追記する

--- text-input-highlight.module.d.ts
+++ text-input-highlight.module.d.ts
@@ -3,6 +3,9 @@ import * as i1 from "./text-input-highlight.component";
 import * as i2 from "./text-input-highlight-container.directive";
 import * as i3 from "./text-input-element.directive";
 import * as i4 from "@angular/common";
+import { NgModule } from "@angular/core";
+
+@NgModule()
 export declare class TextInputHighlightModule {
     static ɵmod: i0.ɵɵNgModuleDefWithMeta<TextInputHighlightModule, [typeof i1.TextInputHighlightComponent, typeof i2.TextInputHighlightContainerDirective, typeof i3.TextInputElementDirective], [typeof i4.CommonModule], [typeof i1.TextInputHighlightComponent, typeof i2.TextInputHighlightContainerDirective, typeof i3.TextInputElementDirective]>;
     static ɵinj: i0.ɵɵInjectorDef<TextInputHighlightModule>;
